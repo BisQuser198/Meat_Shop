0d757e802bceb636459b40c04a8af6012c2838c7
ATP <clmishto3@yahoo.com>
2025-02-01 22:21:10 +0200

    ex50.py works, see comments

--- gotonweb/ex50.py
+++ gotonweb/ex50.py
@@ -12,15 +12,45 @@
 
 ##-------------v2
 
+# from flask import Flask
+# from flask import render_template
+
+# app = Flask(__name__)
+
+# @app.route("/") # when there is a request / ends in / flask will go to def index
+# def index():
+#     greeting = "Hello World"
+#     return render_template("web_form.html", greeting=greeting)
+
+# if __name__ == "__main__":
+#     app.run()
+
+##-------------v3 with web_form.py
+
+# 1) cd E:\python_work\Meat_shop\gotonweb
+# 2) python ex50.py
+# 3) Browser: http://127.0.0.1:5000/hello => action="/hello" -- pulls up web_form.html
+# 4) Method POST can/receives values for var.s name & greet => if request.method == True:
+# 5) return render_template("index.html") => I just wanted to say 'greet' 'name'
+
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/") # when there is a request / ends in / flask will go to def index
+@app.route("/hello", methods=['POST', 'GET'])
 def index():
     greeting = "Hello World"
-    return render_template("index.html", greeting=greeting)
+
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template("index.html", greeting=greeting)
+    else:
+        return render_template("web_form.html")
 
 if __name__ == "__main__":
     app.run()
+
